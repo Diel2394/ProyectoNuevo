1b4feaaa18cfca6316aacca1e57f12529db6172f
Diel2394 <80163824+Diel2394@users.noreply.github.com>
2022-01-15 13:14:30 -0500

    se elabora pagina principal

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//carousel init
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+  
+ var options ={
+     indicator: true,
+     numVisible:5,
+    padding: 15,
+ }
+  
