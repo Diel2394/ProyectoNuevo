b8a56e81e72cf16e5208367e3a54f17a010e550a
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2022-01-15 16:12:05 -0500

    Se realiza el header Mobile First

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 //carousel init
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-  
- var options ={
-     indicator: true,
-     numVisible:5,
-    padding: 15,
- }
-  
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
+
+var options = {
+  indicator: true,
+  numVisible: 5,
+  padding: 15,
+};
+
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", (e) => {
+  menu.classList.toggle("mostrar");
+});
